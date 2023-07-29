20019952c45172dd48a4d5ca9514e3a76b221e23
Jeremiah Pierce <i.r.stuff@gmail.com>
2023-07-29 18:43:01 -0500

    first commit

---
+++ src/main/java/com/jp/calefaction/Calefaction.java
@@ -0,0 +1,38 @@
+package com.jp.calefaction;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.presence.ClientActivity;
+import discord4j.core.object.presence.ClientPresence;
+import discord4j.rest.RestClient;
+
+@SpringBootApplication
+public class Calefaction {
+
+    public static void main(String[] args) {
+        //Start spring application
+        new SpringApplicationBuilder(Calefaction.class)
+            .build()
+            .run(args);
+    }
+
+
+    @Bean
+    public GatewayDiscordClient gatewayDiscordClient() {
+        return DiscordClientBuilder.create(System.getenv("BOT_TOKEN")).build()
+            .gateway()
+            .setInitialPresence(ignore -> ClientPresence.online(ClientActivity.listening("to /commands")))
+            .login()
+            .block();
+    }
+
+    @Bean
+    public RestClient discordRestClient(GatewayDiscordClient client) {
+        return client.getRestClient();
+    }
+
+}
---
+++ src/test/java/com/jp/calefaction/CalefactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.calefaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalefactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
