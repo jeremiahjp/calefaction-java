53c03acab3735e67a8d0bded8a9900a42d350abd
jeremiahjp <jeremiahjpierce@gmail.com>
2023-11-22 12:24:33 -0600

    fix: bumping dependency versions. removing unused code

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,10 @@
+echo "Building container..."
+docker build -t calefaction .
+echo "Stopping all containers ..."
+docker stop $(docker ps -q)
+echo "Removing all containers ..."
+docker rm $(docker ps -aq)
+echo "Running container..."
 docker run -d --env-file=env_vars -p 8080:8080 calefaction
+echo "Pruning..."
+docker image prune -f
