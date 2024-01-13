61d5aa0bc596c84396b986ff2fb2d09dd7431072
jeremiahjp <jeremiahjpierce@gmail.com>
2024-01-12 19:56:15 -0600

    docker-run.sh to always restart

--- docker-run.sh
+++ docker-run.sh
@@ -5,6 +5,6 @@ docker stop $(docker ps -q)
 echo "Removing all containers ..."
 docker rm $(docker ps -aq)
 echo "Running container..."
-docker run -d --env-file=env_vars -p 8080:8080 calefaction
+docker run --restart=always -d --env-file=env_vars -p 8080:8080 calefaction
 echo "Pruning..."
 docker image prune -f
